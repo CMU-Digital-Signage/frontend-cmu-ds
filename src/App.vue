4cfae752b74edf9a72e38b2bba99f1fef335b8dc
thanaporn.ch <thanaporn_chan@cmu.ac.th>
2023-12-17 11:52:51 +0700

    import font sf pro

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,24 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view />
+  <div class="font-sf-pro">
+    <nav v-if="!$route.meta.hideNavbar">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </nav>
+    <router-view />
+  </div>
 </template>
 
 <style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: "SF Pro", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #000000;
 }
 
 nav {
   padding: 30px;
-
   a {
     font-weight: bold;
     color: #2c3e50;
