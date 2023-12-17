e33db43cb9d63a9c27fa8e0d80a17b60895cd95b
thanaporn.ch <thanaporn_chan@cmu.ac.th>
2023-12-17 11:51:44 +0700

    install tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  theme: {
+    textColor: {
+      white: "#FFF",
+      black: "#000",
+    },
+    extend: {},
+    fontFamily: {
+      "sf-pro": ["SF Pro"],
+      "sf-pro-rounded": ["SF Pro Rounded"],
+    },
+    colors: {
+      red: "#FF0000",
+      rose: "#ED3838",
+      periwinkle: "#696CA3",
+      royalBule: "#3671BF",
+      babyBule: "#C2DDFD",
+      turquoise: "#4DF8D7",
+      teal: "#36BFA7",
+      charcoal: "#4A484C",
+      darkGray: "#2D2E2F",
+    },
+  },
+};
