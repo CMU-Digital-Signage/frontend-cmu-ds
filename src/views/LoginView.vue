a8770caf589c6de242882c4bef142eaee02fa2ea
auswitch <90751246+auswitch@users.noreply.github.com>
2023-12-18 14:46:48 +0700

    edit font in LoginPage

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -2,10 +2,10 @@
   <div
     class="w-auto h-96 my-40 mx-60 p-10 justify-center bg-babyBule rounded-md flex flex-col items-center text-center"
   >
-  <span class="text-4xl mb-6 font-bold font-sf-pro">CPE Digital Signage</span>
-    <span class="text-2xl mb-4 font-notoThai">ลงชื่อเข้าสู่ระบบ</span>
+    <span class="text-4xl mb-6 font-bold font-sf-pro">CPE Digital Signage</span>
+    <span class="text-xl mb-2 font-notoThai">ลงชื่อเข้าสู่ระบบ</span>
     <button
-      class="flex items-center justify-center w-96 h-12 relative rounded-[40px] bg-periwinkle font-sf-pro-rounded text-white text-2xl"
+      class="flex items-center justify-center w-96 h-12 relative rounded-[40px] bg-periwinkle font-sf-pro text-white text-2xl"
       @click="$router.push('/')"
     >
       <img
@@ -13,7 +13,7 @@
         alt="cmulogo"
         src="../assets/images/cmu-logo-th.png"
       />
-      <span>Login with CMU Account</span>
+      <span class="text-xl">Login with CMU Account</span>
     </button>
   </div>
 </template>
