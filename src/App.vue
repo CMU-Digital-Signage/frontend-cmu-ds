4aec7a4b876b45e4b6a4b13eb03116f42ee7f846
auswitch <90751246+auswitch@users.noreply.github.com>
2023-12-18 16:59:12 +0700

    edit login page

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,9 @@
 <template>
-  <div class="font-sf-pro">
-    <nav v-if="!$route.meta.hideNavbar">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
-    <router-view />
-  </div>
+  <nav v-if="!$route.meta.hideNavbar">
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </nav>
+  <router-view />
 </template>
 
 <style lang="scss">
--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,20 +1,25 @@
 <template>
-  <div
-    class="w-auto h-96 my-40 mx-60 p-10 justify-center bg-babyBule rounded-md flex flex-col items-center text-center"
-  >
-    <span class="text-4xl mb-6 font-bold font-sf-pro">CPE Digital Signage</span>
-    <span class="text-xl mb-2 font-notoThai">ลงชื่อเข้าสู่ระบบ</span>
-    <button
-      class="flex items-center justify-center w-96 h-12 relative rounded-[40px] bg-periwinkle font-sf-pro text-white text-2xl"
-      @click="$router.push('/')"
+  <div class=" bg-[#f0f0f0] h-screen flex items-center justify-center">
+    <!-- Set height to full screen and center content -->
+    <div
+      class="w-auto h-auto p-24 bg-[#fcfcfc] border-1 border-[#839CBC] drop-shadow-xl rounded-[15px] flex flex-col items-center text-center"
     >
-      <img
-        class="w-7 mr-2"
-        alt="cmulogo"
-        src="../assets/images/cmu-logo-th.png"
-      />
-      <span class="text-xl">Login with CMU Account</span>
-    </button>
+      <img class="w-120 h-20 mb-12" alt="cpelogo" src="../assets/images/cpe-logo.png" />
+
+      <span class="text-4xl mb-6 font-medium text-[#4A484C] font-sf-pro-rounded">Digital Signage</span>
+      <span class="text-lg mb-3 font-notoThai">ลงชื่อเข้าสู่ระบบ</span>
+      <button
+        class="flex items-center justify-center w-auto p-5 h-12 relative rounded-[10px] bg-periwinkle font-sf-pro text-white text-2xl"
+        @click="$router.push('/')"
+      >
+        <img
+          class="w-7 mr-2"
+          alt="cmulogo"
+          src="../assets/images/cmu-logo-th.png"
+        />
+        <span class="text-xl">Login with CMU Account</span>
+      </button>
+    </div>
   </div>
 </template>
 
