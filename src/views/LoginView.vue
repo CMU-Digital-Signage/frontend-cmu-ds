a08e141ad73fc56fa076e126f13929fd54585365
auswitch <90751246+auswitch@users.noreply.github.com>
2023-12-19 00:25:34 +0700

    edit login pg.

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,24 +1,41 @@
 <template>
-  <div class=" bg-[#f0f0f0] h-screen flex items-center justify-center">
-    <!-- Set height to full screen and center content -->
+  <div
+    class="bg-gradient-to-r from-[#E5F9FA] to-[#F6FDF7] h-screen flex items-center justify-center"
+  >
     <div
-      class="w-auto h-auto p-24 bg-[#fcfcfc] border-1 border-[#839CBC] drop-shadow-xl rounded-[15px] flex flex-col items-center text-center"
+      class="relative w-2/3 h-3/4 bg-gradient-to-r from-[#F8FEFE] to-[#F9FDFC] border-2 border-[#B8F1F0] drop-shadow-xl rounded-[15px] flex flex-col items-center text-center"
     >
-      <img class="w-120 h-20 mb-12" alt="cpelogo" src="../assets/images/cpe-logo.png" />
+      <img
+        class="absolute top-0 left-0 w-36 h-11 mt-8 ml-20"
+        alt="cpelogo"
+        src="../assets/images/cpe-logo.png"
+      />
 
-      <span class="text-4xl mb-6 font-medium text-[#4A484C] font-sf-pro-rounded">Digital Signage</span>
-      <span class="text-lg mb-3 font-notoThai">ลงชื่อเข้าสู่ระบบ</span>
-      <button
-        class="flex items-center justify-center w-auto p-5 h-12 relative rounded-[10px] bg-periwinkle font-sf-pro text-white text-2xl"
-        @click="$router.push('/')"
+      <div
+        class="absolute top-40 left-0 w-auto px-14 h-1/2 ml-20  rounded-[15px] flex flex-col items-center justify-center text-center"
       >
-        <img
-          class="w-7 mr-2"
-          alt="cmulogo"
-          src="../assets/images/cmu-logo-th.png"
-        />
-        <span class="text-xl">Login with CMU Account</span>
-      </button>
+        <span
+          class="text-3xl mb-6 font-medium text-[#000000] font-sf-pro-rounded"
+          >Digital Signage</span
+        >
+        <span class="text-lg mb-3 font-notoThai">ลงชื่อเข้าสู่ระบบ</span>
+        <button
+          class="flex items-center justify-center w-auto p-5 h-12 relative rounded-[10px] bg-periwinkle font-sf-pro text-white text-2xl"
+          @click="$router.push('/')"
+        >
+          <img
+            class="w-7 mr-2"
+            alt="cmulogo"
+            src="../assets/images/cmu-logo-th.png"
+          />
+          <span class="text-lg">Login with CMU Account</span>
+        </button>
+      </div>
+      <img
+        class="absolute top-28 right-0 w-1/2"
+        alt="picLogin"
+        src="../assets/images/picLogin.png"
+      />
     </div>
   </div>
 </template>
