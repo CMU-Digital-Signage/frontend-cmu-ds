1c4ffcc749d2faa7dd6e37e172434935c91ee19f
thanaporn.ch <thanaporn_chan@cmu.ac.th>
2024-02-03 02:43:48 +0700

    add calendar

--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -1,24 +1,37 @@
-<template>
-  <div>
-    this is Dashboard page
-    <!-- <TabView>
-      <TabPanel header="kkkk"></TabPanel>
-      <TabPanel header="kkkk"></TabPanel>
-    </TabView> -->
-  </div>
-</template>
-
 <script lang="ts">
 import { defineComponent } from "vue";
-import router from "@/router";
-import store from "@/store";
-import TabView from "primevue/tabview";
-import TabPanel from "primevue/tabpanel";
-
 export default defineComponent({
   name: "DashboardView",
   components: {},
 });
 </script>
+<script setup lang="ts">
+import { ref, reactive, onMounted } from "vue";
+import router from "@/router";
+import store from "@/store";
+import FullCalendar from "@fullcalendar/vue3";
+import { CalendarOptions } from "@fullcalendar/core";
+import dayGridPlugin from "@fullcalendar/daygrid";
+import timeGridPlugin from "@fullcalendar/timegrid";
+
+const fullCalendar = ref(null);
+
+const calendarOptions = reactive({
+  plugins: [dayGridPlugin, timeGridPlugin],
+  initialView: "dayGridMonth",
+  // headerToolbar: false,
+  headerToolbar: {
+    start: "prev,next today",
+    center: "title",
+    end: "dayGridMonth,timeGridDay",
+  },
+  height: innerHeight*0.9,
+}) as CalendarOptions;
+</script>
+<template>
+  <div>
+    <FullCalendar ref="fullCalendar" :options="calendarOptions"></FullCalendar>
+  </div>
+</template>
 
 <style></style>
