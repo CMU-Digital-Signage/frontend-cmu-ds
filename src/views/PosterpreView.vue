8199003812719c554caae02e9acbfc1f04d8b431
thidayu <tanthidayu_0000@hotmail.com>
2024-02-10 21:54:33 +0700

    add preview

---
+++ src/views/PosterpreView.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import PreviewComponent from "@/components/PreviewComponent.vue";
+</script>
+
+<template>
+  <div class="">
+    <PreviewComponent />
+  </div>
+</template>
+
+<style></style>
