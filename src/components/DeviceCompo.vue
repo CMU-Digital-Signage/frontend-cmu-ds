aa9c642dac9aa6be9e4d414ae78060eae894da93
thanaporn.ch <thanaporn_chan@cmu.ac.th>
2024-07-10 22:55:22 +0700

    #fixed: ModalAddEditDevice

--- src/components/DeviceCompo.vue
+++ src/components/DeviceCompo.vue
@@ -7,13 +7,9 @@ export default defineComponent({
 <script setup lang="ts">
 import { ref, reactive, computed } from "vue";
 import store from "@/store";
-import { editDevice, deleteDevice } from "@/services";
+import { deleteDevice } from "@/services";
 import { Device } from "@/types";
-import {
-  initialFormDevice,
-  onUpload,
-  calculateScreenHeight,
-} from "@/utils/constant";
+import { initialFormDevice, calculateScreenHeight } from "@/utils/constant";
 import { useToast } from "primevue/usetoast";
 import ModalAddEditDevice from "@/components/Modal/ModalAddEditDevice.vue";
 
@@ -29,6 +25,10 @@ const toggleOverlay = (e: any, panel: any) => {
   panel.toggle(e);
 };
 
+const closeModalAddEditDevice = () => {
+  showPopup.value = false;
+};
+
 const del = async () => {
   loading.value = true;
   const res = await deleteDevice(selectDelDevice.value?.MACaddress || "");
@@ -41,10 +41,6 @@ const del = async () => {
   loading.value = false;
   deletePopup.value = false;
   selectDelDevice.value = undefined;
-};
-
-const closeModalAddEditDevice = () => {
-  showPopup.value = false;
 };
 </script>
 
@@ -212,198 +208,6 @@ const closeModalAddEditDevice = () => {
     :dataEdit="form"
     :onClose="closeModalAddEditDevice"
   />
-  <!-- <Dialog
-    v-model:visible="showPopup"
-    header="Edit Device"
-    class="w-[600px] h-auto"
-    modal
-    :draggable="false"
-    :closable="!loading"
-    close-on-escape
-    @after-hide="Object.assign(form, initialFormDevice)"
-    :pt="{
-      content: {
-        style:
-          'border-bottom-left-radius: 20px; border-bottom-right-radius: 20px; ',
-      },
-      header: {
-        style: 'border-top-left-radius: 20px; border-top-right-radius: 20px;  ',
-      }
-    }"
-  >
-    <div class="flex flex-col gap-2 h-[90px]">
-      <div class="inline-block">
-        <label for="deviceName" class="text-primary-50 font-medium">
-          Device Name
-        </label>
-        <label for="deviceName" class="text-[#FF0000] font-medium"> * </label>
-      </div>
-      <InputText
-        v-model:model-value="form.deviceName"
-        @keydown="
-          (e) => {
-            limitCharDevice = form.deviceName?.length === 8;
-          }
-        "
-        class="border border-[#C6C6C6] p-2 text-primary-50 w-full rounded-lg mb-3"
-        placeholder="Max 8 Character Ex.CPE01 "
-        maxlength="8"
-        :class="{
-          'border-red-500 shadow-none':
-            form.deviceName?.length === 8 && limitCharDevice,
-        }"
-        id="deviceName"
-      ></InputText>
-      <div class="text-red-500 -mt-5 text-[12px]" tyle="min-height: 1rem;">
-        <div v-if="form.deviceName?.length === 8 && limitCharDevice">
-          You have reached the character limit.
-        </div>
-      </div>
-    </div>
-    <div class="flex flex-col gap-2 h-[90px]">
-      <div class="inline-block">
-        <label for="deviceName" class="text-primary-50 font-medium">
-          MAC Address
-        </label>
-        <label for="deviceName" class="text-[#FF0000] font-medium">*</label>
-      </div>
-      <InputText
-        v-model="form.MACaddress"
-        class="border border-[#C6C6C6] p-2 text-primary-50 w-full rounded-lg mb-3 cursor-not-allowed"
-        disabled
-      ></InputText>
-    </div>
-    <div class="flex flex-col gap-2 h-[90px]">
-      <div class="inline-block">
-        <label for="macAddress" class="text-primary-50 font-medium">
-          Room
-        </label>
-        <label for="deviceName" class="text-[#FF0000] font-medium"> * </label>
-      </div>
-      <InputText
-        v-model:model-value="form.room"
-        @keydown="
-          (e) => {
-            limitCharRoom = form?.room?.length === 3;
-            if (e.key !== 'Backspace' && !/^\d$/.test(e.key)) {
-              isNumber = false;
-              e.preventDefault();
-            } else {
-              isNumber = true;
-            }
-          }
-        "
-        class="border border-[#C6C6C6] p-2 text-primary-50 w-full rounded-lg mb-3"
-        placeholder="Number only Ex.516"
-        maxlength="3"
-        :class="{
-          'border-red-500 shadow-none':
-            (form?.room?.length && form.room.length >= 3 && limitCharRoom) ||
-            !isNumber,
-        }"
-      ></InputText>
-      <div class="text-red-500 -mt-5 text-[12px]" tyle="min-height: 1rem;">
-        <div
-          v-if="form?.room?.length && form.room.length >= 3 && limitCharRoom"
-        >
-          You have reached the character limit.
-        </div>
-        <div v-else-if="!isNumber">Type the number using digits 0-9 only.</div>
-      </div>
-    </div>
-    <div class="flex flex-col gap-1 h-[90px]">
-      <label for="macAddress" class="text-primary-50 font-medium">
-        Location Description
-      </label>
-      <InputText
-        v-model="form.description"
-        class="border border-[#C6C6C6] p-2 text-primary-50 w-full rounded-lg mb-3"
-        placeholder="(Optional)"
-      ></InputText>
-    </div>
-    <div class="flex flex-col gap-1">
-      <label for="macAddress" class="text-primary-50 font-medium">
-        Location Photo
-      </label>
-      <FileUpload
-        accept="image/*"
-        :show-upload-button="false"
-        :show-cancel-button="false"
-        :multiple="false"
-        @select="
-          async (e) => {
-            if (e.files[0]) form.location = await onUpload(e.files[0]);
-            else errorSelectFile();
-          }
-        "
-      >
-        <template #header="{ chooseCallback, clearCallback }">
-          <div class="flex items-center">
-            <Button
-              @click="
-                clearCallback();
-                chooseCallback();
-              "
-              icon="pi pi-plus"
-              label="Choose File"
-              rounded
-              outlined
-            ></Button>
-          </div>
-        </template>
-        <template #content="{ removeFileCallback }">
-          <div
-            v-if="form.location"
-            class="flex justify-between items-center w-full"
-          >
-            <img
-              alt="locationImage"
-              :src="form.location.dataURL"
-              class="w-2/4 h-2/4"
-            />
-            <Button
-              icon="pi pi-times"
-              @click="
-                removeFileCallback(0);
-                form.location = null;
-              "
-              outlined
-              rounded
-              severity="danger"
-            />
-          </div>
-          <div v-else></div>
-        </template>
-        <template #empty>
-          <div
-            v-if="!form.location"
-            class="flex flex-col text-center items-center"
-          >
-            <i
-              class="pi pi-cloud-upload border-2 rounded-full text-8xl w-fit p-5"
-            />
-            <p class="mt-4 mb-0">Drag and drop files to here to upload.</p>
-          </div>
-        </template>
-      </FileUpload>
-    </div>
-    <div class="flex flex-row gap-4 pt-3">
-      <Button
-        label="Cancel"
-        text
-        :loading="loading"
-        @click="showPopup = false"
-        class="secondaryButton"
-      ></Button>
-      <Button
-        label="Done"
-        :loading="loading"
-        class="primaryButton"
-        @click="edit"
-        :disabled="!form.deviceName || !form.room"
-      ></Button>
-    </div>
-  </Dialog> -->
 </template>
 
 <style scoped>
