7fee1c73ca8b1bb02954e85e962f50ef0f2f4eba
auswitch <90751246+auswitch@users.noreply.github.com>
2024-07-21 03:01:35 +0700

    add vif

--- src/views/cmuOAuthCallbackView.vue
+++ src/views/cmuOAuthCallbackView.vue
@@ -1,22 +1,68 @@
 <template>
-  <div class="flex  h-full  justify-center items-center flex-col ">
-    <img
-              class=" w-[500px] mb-10 "
-              alt="cmulogo"
-              src="../assets/images/403.png"
-            />
-            <p class=" text-[36px] text-teal-500 font-semibold mb-4">Access Denied</p>
-            <p class=" text-[18px] text-[#575757] font-normal">Look like your CMU account don't have permission to access this page.  </p>
-            <p class=" text-[18px] text-[#575757] font-normal mb-14 mt-[2px]">Reach out to system adminstrator to gain access </p>
-    <div class="justify-center flex flex-row items-center text-xl">
-   
-      <Button
-        class=" text-cyan-600 bg-none border-none font-normal  text-xl hover:underline "
-        link
-        @click="$router.push('/login')"
-        >Back to login</Button
-      >
+  <div v-if="status != 0" class="flex h-full justify-center items-center flex-col">
+    <div v-if="[403, 401].includes(status)">
+      <img
+        class="w-[500px] mb-10"
+        alt="cmulogo"
+        src="../assets/images/403.png"
+      />
+      <p class="text-[36px] text-teal-500 font-semibold mb-4">Access Denied</p>
+      <p class="text-[18px] text-[#575757] font-normal">
+        Look like your CMU account don't have permission to access this page.
+      </p>
+      <p class="text-[18px] text-[#575757] font-normal mb-14 mt-[2px]">
+        Reach out to system adminstrator to gain access
+      </p>
     </div>
+    <div v-else-if="[400].includes(status)">
+      <img
+        class="w-[500px] mb-10"
+        alt="cmulogo"
+        src="../assets/images/403.png"
+      />
+      <p class="text-[36px] text-teal-500 font-semibold mb-4">Bad Request</p>
+      <p class="text-[18px] text-[#575757] font-normal">
+        Look like your CMU account don't have permission to access this page.
+      </p>
+      <p class="text-[18px] text-[#575757] font-normal mb-14 mt-[2px]">
+        Reach out to system adminstrator to gain access
+      </p>
+    </div>
+    <div v-else-if="[500].includes(status)">
+      <img
+        class="w-[500px] mb-10"
+        alt="cmulogo"
+        src="../assets/images/403.png"
+      />
+      <p class="text-[36px] text-teal-500 font-semibold mb-4">Internal server error</p>
+      <p class="text-[18px] text-[#575757] font-normal">
+        Look like your CMU account don't have permission to access this page.
+      </p>
+      <p class="text-[18px] text-[#575757] font-normal mb-14 mt-[2px]">
+        Reach out to system adminstrator to gain access
+      </p>
+    </div>
+    <div v-else-if="[401].includes(status)">
+      <img
+        class="w-[500px] mb-10"
+        alt="cmulogo"
+        src="../assets/images/403.png"
+      />
+      <p class="text-[36px] text-teal-500 font-semibold mb-4">Unauthorized</p>
+      <p class="text-[18px] text-[#575757] font-normal">
+        Look like your CMU account don't have permission to access this page.
+      </p>
+      <p class="text-[18px] text-[#575757] font-normal mb-14 mt-[2px]">
+        Reach out to system adminstrator to gain access
+      </p>
+    </div>
+
+    <Button
+      class="text-cyan-600 bg-none border-none font-normal text-xl hover:underline"
+      link
+      @click="$router.push('/login')"
+      >Back to login</Button
+    >
   </div>
 </template>
 
@@ -34,6 +80,8 @@ export default {
     const store = useStore();
     const message = ref("Loading...");
 
+    const status = ref(0);
+
     async function signIn(authorizationCode: string) {
       try {
         const res = await axios.post(
@@ -46,12 +94,14 @@ export default {
             withCredentials: true,
           }
         );
+        status.value = res.status;
         return res.data;
       } catch (err: any) {
         if (!err.response) {
-          message.value =
-            `Cannot connect to API Server.\nPlease try again later.`;
+          status.value = 500;
+          message.value = `Cannot connect to API Server.\nPlease try again later.`;
         } else if (!err.response.data.ok) {
+          status.value = err.response.status;
           message.value = err.response.data.message;
         } else {
           message.value = "Unknown error occurred. Please try again later.";
@@ -80,6 +130,7 @@ export default {
     return {
       message,
       userInfo: store.state.userInfo,
+      status,
     };
   },
 };
