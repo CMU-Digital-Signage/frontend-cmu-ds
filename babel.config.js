1fb9bcc9d10f514790d6ccd954a7ffcb25ac3284
thanaporn.ch <thanaporn_chan@cmu.ac.th>
2024-12-02 14:19:05 +0700

    #fixed: old web browser

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,6 @@
 module.exports = {
-  presets: ["@vue/cli-plugin-babel/preset"],
+  presets: [
+    "@vue/cli-plugin-babel/preset",
+    ["@babel/preset-env", { targets: "> 0.5%, last 2 versions, not dead" }],
+  ],
 };
